ae6c9285796a100dc43dcb90247cc3cba2de12e6
gabriel <gabrieldecarvalhu@gmail.com>
2024-04-25 13:30:06 -0300

    projeto estático e responsivo pronto

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Services from './componentes/Services'
 import Infos from './componentes/Infos'
 import Sobre from './componentes/Sobre'
 import Times from './componentes/Times'
+import Bottom from './componentes/Bottom'
 
 function App() {
 
@@ -15,6 +16,7 @@ function App() {
       <Infos/>
       <Sobre/>
       <Times/>
+      <Bottom/>
     </>
   )
 }
---
+++ src/componentes/Bottom/index.jsx
@@ -0,0 +1,62 @@
+import './bottom.css'
+import letstalk from '../../assets/img/letstalk.svg'
+import logo_arthur from '../../assets/img/logo_arthur.svg'
+import cttn from '../../assets/img/cttn.svg'
+import contact from '../../assets/img/contact.svg'
+import instagram from '../../assets/img/insta2.png'
+import save from '../../assets/img/save2.png'
+import dri from '../../assets/img/dri2.png'
+
+
+
+
+
+export default function Bottom() {
+  return (
+    <footer className='image_bottom'>
+      <div className='container'>
+        <div className='talk'>
+          <img src={letstalk} alt='' />
+        </div>
+
+        <div className='bottom_grid'>
+          <div className='grid_esquerda'>
+            <h1>HELLO@COTTONFILMS.COM</h1>
+            <img src={logo_arthur} alt='' />
+          </div>
+
+          <div className='grid_direita'>
+
+            <div className='grid_contatos'>
+              <div className='cttn'>
+                <img src={cttn} alt='' />
+              </div>
+
+              <div className='contatos_flex'>
+                <ul className='lista_flex'>
+                  <li>
+                    <img src={contact} alt='' />
+                  </li>
+                  <li>
+                    <img src={instagram} alt='' />
+                  </li>
+                  <li>
+                    <img src={save} alt='' />
+                  </li>
+                  <li>
+                    <img src={dri} alt='' />
+                  </li>
+                </ul>
+
+              </div>
+
+            </div>
+
+
+
+          </div>
+        </div>
+      </div>
+    </footer>
+  )
+}
