41282ba3cf41d7893b7a220ff6879adf41bb16e3
gabriel <gabrieldecarvalhu@gmail.com>
2024-06-03 20:24:07 -0300

    add material ui e tailwind

--- src/componentes/TopoPagina/index.jsx
+++ src/componentes/TopoPagina/index.jsx
@@ -1,17 +1,25 @@
-import banner from '../../assets/img/img_1.png'
+import React from 'react'
+import ReactPlayer from 'react-player'
+
 import cotonFilms from '../../assets/img/cotonFilms.png'
-import videos from '../../assets/img/videos.png'
 import frame from '../../assets/img/Frame1.png'
-import video1 from '../../assets/videos/video1.mp4'
+import video1 from '../../../public/videos/video1.mp4'
+import video2 from '../../../public/videos/video2.mp4'
+import video3 from '../../assets/videos/video3.mp4'
 import Menu from '../Menu/index.jsx'
 import './topoPagina.css'
 import '../../grid.css'
+import { Progress } from "@/components/ui/progress"
 
 import { gsap } from "gsap";
 import { ScrollTrigger } from 'gsap/ScrollTrigger'
 import Lenis from '@studio-freight/lenis'
 import SplitType from 'split-type'
-import { useEffect } from 'react'
+import { useEffect, useRef, useState } from 'react'
+
+
+
+
 
 
 
@@ -50,14 +58,14 @@ export default function TopoPagina() {
         isMobile: "(max-width:480px)",
         isDesktop: "(min-width:481px)"
       }, (context) => {
-        let {isMobile, isDesktop} = context.conditions;
+        let { isMobile, isDesktop } = context.conditions;
 
 
         gsap.from(text.chars, {
           scrollTrigger: {
             trigger: char,
-            start: isMobile ? "top 250px" :'top 80%',
-            end: isMobile ?  "bottom 100px" : 'top 20%',
+            start: isMobile ? "top 250px" : 'top 80%',
+            end: isMobile ? "bottom 100px" : 'top 20%',
             scrub: true,
             //markers: true
           },
@@ -71,6 +79,53 @@ export default function TopoPagina() {
   }, [])
 
 
+  const videos = [
+    {
+      video: video1,
+      id: 1
+    },
+    {
+      video: video2,
+      id: 2
+    }
+  ]
+  
+  const [currentVideoIndex, setCurrentVideoIndex] = useState(0);
+  const [duracao, setDuracao] = useState(0)
+  
+    const [isPlaying, setIsPlaying] = useState(true)
+
+  
+  
+
+  function handleEnded() {
+    setCurrentVideoIndex((prevIndex) =>
+      (prevIndex + 1) % videos.length
+    );
+  }
+
+
+  function handleOnProgress(progress) {
+    setDuracao(progress.playedSeconds)
+    console.log(duracao)
+   
+
+  }
+  function handleError(e) {
+    console.error('Error playing video:', e);
+  }
+
+  // function handlePlay () {
+  //   console.log('onPlay')
+  //   setIsPlaying(true)
+  // }
+  //   function handleSeekChange (e){
+  //   setDuracao(parseFloat(e.target.value))
+  // }
+
+
+
+
 
 
 
@@ -80,7 +135,17 @@ export default function TopoPagina() {
     <>
       <header className="home">
         <Menu />
-        <video className="imagem_banner" src={video1} autoPlay loop muted alt='' />
+        <ReactPlayer className="imagem_banner"
+          url={videos[currentVideoIndex].video}
+          alt=''
+          playing
+          width='1920px'
+          height='1080px'
+          controls={true}
+          muted={true}
+          onProgress={handleOnProgress}
+          onEnded={handleEnded}
+        />
 
         <div className='container'>
           <div className="img_films">
@@ -88,7 +153,25 @@ export default function TopoPagina() {
           </div>
 
           <div className="video">
-            <img src={videos} alt='imagem banner' />
+            <div className='video_border'>
+              <h3>Video 1</h3>
+              <h3>[2024]</h3>
+            </div>
+            <div className='video_time'>
+              <Progress value={33} />
+            </div>
+          </div>
+          <div className="video">
+            <div className='video_border'>
+              <h3>Video 2</h3>
+              <h3>[2023]</h3>
+            </div>
+          </div>
+          <div className="video">
+            <div className='video_border'>
+              <h3>Video 3</h3>
+              <h3>[2022]</h3>
+            </div>
           </div>
 
         </div>
@@ -102,6 +185,8 @@ export default function TopoPagina() {
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint eum rerum dolore. Consequatur, quisquam? Repudiandae libero consequatur
               praesentium reprehenderit enim sed facere velit, id, eligendi numquam delectus maiores porro cumque?</p>
           </div>
+          
+          
 
           <div className="imagem_grid">
             <img src={frame} alt='' />
