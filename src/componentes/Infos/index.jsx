ddadea501b9e0d3faea35067e9293367319ff60a
Gabriel Barbosa <gabrieldecarvalhu@gmail.com>
2025-06-24 17:23:56 -0300

    responsive infos

--- src/componentes/Infos/index.jsx
+++ src/componentes/Infos/index.jsx
@@ -2,54 +2,58 @@ import './infos.css'
 import '../../grid.css'
 export default function Infos() {
     return (
-        <div className='container'>
-            <div className='cards'>
-                <div className='card'>
-                    <div className='card_top'>
-                        <p>countries</p>
-                        <p>01</p>
-                    </div>
-                    <h1>08</h1>
-                </div>
+        <section className='container-infos'>
+            <div className='grid-global'>
 
-                <div className='card'>
-                    <div className='card_top'>
-                        <p>clientes</p>
-                        <p>02</p>
+
+                <div className='cards'>
+                    <div className='card'>
+                        <div className='card_top'>
+                            <p>countries</p>
+                            <p>01</p>
+                        </div>
+                        <h1>08</h1>
                     </div>
-                    <div className='card_bot'>
-                        <h1>+128</h1>
+
+                    <div className='card'>
+                        <div className='card_top'>
+                            <p>clientes</p>
+                            <p>02</p>
+                        </div>
+                        <div className='card_bot'>
+                            <h1>+128</h1>
+                        </div>
+                    </div>
+                    <div className='card'>
+                        <div className='card_top'>
+                            <p>countries</p>
+                            <p>01</p>
+                        </div>
+                        <h1>08</h1>
+                    </div>
+                    <div className='card'>
+                        <div className='card_top'>
+                            <p>clientes</p>
+                            <p>02</p>
+                        </div>
+                        <div className='card_bot'>
+                            <h1>+128</h1>
+                        </div>
                     </div>
                 </div>
-                <div className='card'>
-                    <div className='card_top'>
-                        <p>countries</p>
-                        <p>01</p>
+                <div className='texto_info'>
+                    <div>
+
                     </div>
-                    <h1>08</h1>
-                </div>
-                <div className='card'>
-                    <div className='card_top'>
-                        <p>clientes</p>
-                        <p>02</p>
-                    </div>
-                    <div className='card_bot'>
-                        <h1>+128</h1>
+                    <div className='text'>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed a
+                            luctus metus. Morbi fermentum nulla vel nunc vulputate fringilla. Etiam suscipit
+                            porttitor neque, in fringilla libero. Lorem ipsum dolor sit amet,
+                            consectetur adipiscing elit. Sed a luctus metus. Morbi fermentum nulla vel nunc vulputate
+                            fringilla. Etiam suscipit porttitor neque, in fringilla libero. </p>
                     </div>
                 </div>
             </div>
-            <div className='texto_info'>
-                <div>
-
-                </div>
-                <div className='text'>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed a 
-                        luctus metus. Morbi fermentum nulla vel nunc vulputate fringilla. Etiam suscipit 
-                        porttitor neque, in fringilla libero. Lorem ipsum dolor sit amet, 
-                        consectetur adipiscing elit. Sed a luctus metus. Morbi fermentum nulla vel nunc vulputate 
-                        fringilla. Etiam suscipit porttitor neque, in fringilla libero. </p>
-                </div>
-            </div>
-        </div>
+        </section>
     )
 }
