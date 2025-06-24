2d90760869300d4665778688558a4acc677d4936
Gabriel Barbosa <gabrieldecarvalhu@gmail.com>
2025-06-24 19:41:54 -0300

    fix: pin

--- src/componentes/Bottom/index.jsx
+++ src/componentes/Bottom/index.jsx
@@ -27,61 +27,64 @@ export default function Bottom() {
   return (
     <>
       <section className='image_bottom'>
-        <div className='container-bottom'>
-          <div className='talk'>
-            <img src={letstalk} alt='' />
-          </div>
+        <div className='grid-global'>
 
-          <div className='bottom_grid'>
-            <div className='grid_esquerda'>
-              <h1>HELLO@COTTONFILMS.COM</h1>
-              <img src={logo_arthur} alt='' />
+
+          <div className='container-bottom'>
+            <div className='talk'>
+              <img src={letstalk} alt='' />
             </div>
 
-            <div className='grid_direita'>
-
-              <div className='grid_contatos'>
-                <div className='cttn'>
-                  <img src={cttn} alt='' />
-                </div>
-
-                <div className='contatos_flex'>
-                  <ul className='lista_flex'>
-                    <li>
-                      <img src={contact} alt='' />
-                    </li>
-                    <li>
-                      <motion.img
-                        src={instagram}
-                        alt=''
-                        whileHover={{ scale: 1.1 }}
-                        transition={{ duration: 0.2 }}
-                      />
-                    </li>
-                    <li>
-                      <motion.img
-                        src={save}
-                        alt=''
-                        whileHover={{ scale: 1.1 }}
-                        transition={{ duration: 0.2 }}
-                      />
-                    </li>
-                    <li>
-                      <motion.img
-                        src={dri}
-                        alt=''
-                        whileHover={{ scale: 1.1 }}
-                        transition={{ duration: 0.2 }}
-                      />
-                    </li>
-                  </ul>
-
-                </div>
-
+            <div className='bottom_grid'>
+              <div className='grid_esquerda'>
+                <h1>HELLO@COTTONFILMS.COM</h1>
+                <img src={logo_arthur} alt='' />
               </div>
 
+              <div className='grid_direita'>
+
+                <div className='grid_contatos'>
+                  <div className='cttn'>
+                    <img src={cttn} alt='' />
+                  </div>
+
+                  <div className='contatos_flex'>
+                        <img src={contact} alt='' />
+                    <ul className='lista_flex'>
+                      
+                      <li>
+                        <motion.img
+                          src={instagram}
+                          alt=''
+                          whileHover={{ scale: 1.1 }}
+                          transition={{ duration: 0.2 }}
+                        />
+                      </li>
+                      <li>
+                        <motion.img
+                          src={save}
+                          alt=''
+                          whileHover={{ scale: 1.1 }}
+                          transition={{ duration: 0.2 }}
+                        />
+                      </li>
+                      <li>
+                        <motion.img
+                          src={dri}
+                          alt=''
+                          whileHover={{ scale: 1.1 }}
+                          transition={{ duration: 0.2 }}
+                        />
+                      </li>
+                    </ul>
+
+                  </div>
+
+                </div>
 
 
+
+              </div>
             </div>
           </div>
         </div>
