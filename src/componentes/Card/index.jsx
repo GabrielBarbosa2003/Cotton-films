616d0a4d517fb51d3d73e06b3df1d8bb3013bacb
Gabriel Barbosa <gabrieldecarvalhu@gmail.com>
2025-06-24 11:48:20 -0300

    text in cards of galery

--- src/componentes/Card/index.jsx
+++ src/componentes/Card/index.jsx
@@ -14,11 +14,22 @@ export default function Card({ img }) {
         <div className="quadrados"
             onMouseEnter={() => setHover(true)}
             onMouseLeave={() => setHover(false)}>
+                <div className='project-galery'>
+                    <div className='galery-left'>
+                        <p>NAME OF PROJECT</p>
+                        <p><span>(2023.02)</span></p>
+                    </div>
+                    <div className='galery-right'>
+                    <p>DIRECT BY</p>
+                    <p>ARTHUR MEDEIROS</p>
+                    </div>
+                </div>
+
             <motion.img
                 src={img} alt=''
-                whileHover={{ scale: 1.2}}
-                transition={{ duration: 0.5 }}
-            />
+                whileHover={{ scale: 1.2 }}
+                transition={{ duration: 0.5 }} />
+
             <div className={hover ? 'case element' : ' case element hidden'}>
                 <img src={ver_case} alt='' />
             </div>
